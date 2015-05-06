3460334c6054061a041f80a3a731643ed62a547e
Daniel-Davis <ddv9461@saintjoe.edu>
2015-05-06 11:36:53 -0500

    Update date.js

--- date.js
+++ date.js
@@ -1,8 +1,8 @@
 function findDate(string) {
   var dateTime = /(\d{1,2})-(\d{1,2})-(\d{4})/;
   var match = dateTime.exec(string);
-  return new Date(Number(match[2] - 1),
-                  Number(match[3]),
+  return new Date(Number(match[2] - 1), // switched this 
+                  Number(match[3]), // with this
                   Number(match[1]));
 }
 print(findDate("30-1-2003"));
